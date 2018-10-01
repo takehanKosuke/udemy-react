9655f55426802e0f171951d31859e9f07ef86880
takehanKosuke <kkoussuke530@icloud.com>
2018-10-02 00:31:29 +0900

    larne jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
-  render() {
-    return <h1>Hello World</h1>;
+  render(){
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked")}} />
+      </React.Fragment>
+    )
   }
 }
 
